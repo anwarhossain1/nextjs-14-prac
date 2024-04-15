d6a6da024203b08839688265d1efad2263e337a8
Anwar Hossain <irishkhan33@gmail.com>
2024-04-15 15:37:01 +0600

    changes added

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,6 +1,10 @@
 import Link from "next/link";
+import { notFound } from "next/navigation";
 
 const page = ({ params }: { params: { productId: string } }) => {
+  if (parseInt(params.productId) > 1000) {
+    notFound();
+  }
   return (
     <div>
       <Link href={`/products/${params.productId}/reviews/${params.productId}`}>
