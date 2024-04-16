00c0525cca1e3fe8057971b3a982e191af68915d
anwarhossain1 <irishkhan33@gmail.com>
2024-04-16 14:18:04 +0600

    dynamic metadata usage mechanism added

--- src/app/products/[productId]/page.tsx
+++ src/app/products/[productId]/page.tsx
@@ -1,6 +1,12 @@
+import { Metadata } from "next";
 import Link from "next/link";
 import { notFound } from "next/navigation";
 
+export const generateMetadata = ({ params }): Metadata => {
+  return {
+    title: `Product ${params.productId}`,
+  };
+};
 const page = ({ params }: { params: { productId: string } }) => {
   if (parseInt(params.productId) > 1000) {
     notFound();
