eca1959de735cb14c5742073cd7da31a124cac7d
anwarhossain1 <irishkhan33@gmail.com>
2024-04-16 13:27:54 +0600

    static metadata mechanism added

---
+++ src/app/about/page.tsx
@@ -0,0 +1,9 @@
+export const metadata = {
+  title: "Next 14 practice",
+  description: "This is a simply about me page",
+};
+const page = () => {
+  return <div>About me</div>;
+};
+
+export default page;
