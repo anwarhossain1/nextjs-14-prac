ff80664ca0d348a51c0618b1ac4da2bac2d1865b
anwarhossain1 <irishkhan33@gmail.com>
2024-04-21 17:24:15 +0600

    root level intercepting route mechanism added

---
+++ src/app/f1/f4/(...)about/page.tsx
@@ -0,0 +1,5 @@
+const page = () => {
+  return <div>(...) root level intercepted about me page</div>;
+};
+
+export default page;
--- src/app/f1/f4/page.tsx
+++ src/app/f1/f4/page.tsx
@@ -2,9 +2,10 @@ import Link from "next/link";
 
 const page = () => {
   return (
-    <div>
+    <div className="flex flex-col gap-3">
       <h2>this is f4</h2>
       <Link href="/f1/f3">go f3</Link>
+      <Link href="/about">about</Link>
     </div>
   );
 };
